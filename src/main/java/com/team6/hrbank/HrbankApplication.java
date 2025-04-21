080bada16f2ca30d363bcab913de43da0eb7bbff
hanna kong <khn1346@gmail.com>
2025-04-21 18:20:48 +0900

    init commit

---
+++ src/main/java/com/team6/hrbank/HrbankApplication.java
@@ -0,0 +1,13 @@
+package com.team6.hrbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/hrbank/HrbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.hrbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
