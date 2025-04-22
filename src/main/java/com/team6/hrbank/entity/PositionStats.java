bd1f7cdf950fbf4ca304e90c7032fd0f835ea38d
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-22 20:52:30 +0900

    feat: PositionStats 엔티티 및 PositionStatsRepository 생성 #34

---
+++ src/main/java/com/team6/hrbank/entity/PositionStats.java
@@ -0,0 +1,65 @@
+package com.team6.hrbank.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.Instant;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "position-stats",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uq_position_state_date",
+            columnNames = {"stat_date", "position_name", "employee_state"}
+        )
+    })
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class PositionStats {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "employee_count", nullable = false)
+  private long employeeCount;
+
+  @Column(name = "joined_employee_count", nullable = false)
+  private long joinedCount;
+
+  @Column(name = "left_employee_count", nullable = false)
+  private long leftCount;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Instant createdAt;
+
+  @Column(name = "stat_date", nullable = false)
+  private LocalDate statDate;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "position_name", nullable = false)
+  private EmployeePosition positionName;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "employee_state", nullable = false)
+  private EmployeeState employeeState;
+
+}
---
+++ src/main/java/com/team6/hrbank/repository/PositionStatsRepository.java
@@ -0,0 +1,13 @@
+package com.team6.hrbank.repository;
+
+import com.team6.hrbank.entity.EmployeeState;
+import com.team6.hrbank.entity.PositionStats;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PositionStatsRepository extends JpaRepository<PositionStats, Long> {
+
+  // 넘어온 조회 날짜 + 상태를 기준으로 조회하여 직무 별 PositionStats List 반환
+  List<PositionStats> findAllByStatDateAndEmployeeState(LocalDate statDate, EmployeeState employeeState);
+}
