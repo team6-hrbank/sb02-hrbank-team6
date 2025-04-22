72e531e595e7b008c3dc217e476478303d170f3c
hanna kong <khn1346@gmail.com>
2025-04-22 14:01:40 +0900

    feat: 에러 코드 정의용 enum 추가

---
+++ src/main/java/com/team6/hrbank/exception/ErrorCode.java
@@ -0,0 +1,36 @@
+package com.team6.hrbank.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum ErrorCode implements Code {
+
+  NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스를 찾을 수 없습니다."),
+  BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+  FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+
+  //EMPLOYEE
+  EMPLOYEE_NOT_FOUND(HttpStatus.NOT_FOUND, "직원을 찾을 수 없습니다."),
+  DUPLICATE_EMPLOYEE(HttpStatus.CONFLICT, "이미 존재하는 직원입니다."),
+
+  //DEPARTMENT
+  DEPARTMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "부서를 찾을 수 없습니다."),
+  DUPLICATE_DEPARTMENT(HttpStatus.CONFLICT, "이미 존재하는 부서입니다.");
+
+
+  private final HttpStatus status;
+  private final String message;
+
+  @Override
+  public HttpStatus getStatus() {
+    return this.status;
+  }
+
+  @Override
+  public String getMessage() {
+    return this.message;
+  }
+
+}
