c3d7c3a672fd8e5d8d9f3bd9e5412c0a22d25a06
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-22 21:00:41 +0900

    feat: DepartmentStatsService 생성 #55

---
+++ src/main/java/com/team6/hrbank/service/DepartmentStatsService.java
@@ -0,0 +1,13 @@
+package com.team6.hrbank.service;
+
+import com.team6.hrbank.entity.EmployeeState;
+
+public interface DepartmentStatsService {
+  // 배치 처리
+  void createTodayStats();
+
+  // 배치 처리 내부에서 쓰이는 상태별 저장 로직
+  void createTodayStatsByState(EmployeeState state);
+
+
+}
