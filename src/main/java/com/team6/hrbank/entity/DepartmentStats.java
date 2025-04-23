ac27c6c939033541e59717a9be1caba0aa36860b
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-23 21:08:47 +0900

    chore: 엔티티 Table 이름 수정

--- src/main/java/com/team6/hrbank/entity/DepartmentStats.java
+++ src/main/java/com/team6/hrbank/entity/DepartmentStats.java
@@ -20,7 +20,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name = "department-stats",
+@Table(name = "department_stats",
     uniqueConstraints = {
         @UniqueConstraint(
             name = "uq_department_state_date",
--- src/main/java/com/team6/hrbank/entity/PositionStats.java
+++ src/main/java/com/team6/hrbank/entity/PositionStats.java
@@ -20,7 +20,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name = "position-stats",
+@Table(name = "position_stats",
     uniqueConstraints = {
         @UniqueConstraint(
             name = "uq_position_state_date",
