43a40d2d1ef6786e459771903ea740ec8034c3f9
smthswt <hin19823@gmail.com>
2025-04-23 13:28:37 +0900

    #28 FileMetadata 인터페이스 create 추가

--- src/main/java/com/team6/hrbank/service/FileMetadataService.java
+++ src/main/java/com/team6/hrbank/service/FileMetadataService.java
@@ -4,10 +4,11 @@ import com.team6.hrbank.entity.FileMetadata;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface FileMetadataService {
 
   Optional<FileMetadata> findById(Long id);
-
+  FileMetadata create(MultipartFile file);
 
 }
