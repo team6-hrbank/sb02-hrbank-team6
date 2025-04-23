7eda89083cfb85793bdd0e7d3e4f779fa8898383
hyorim <tg305@naver.com>
2025-04-23 17:23:21 +0900

    feat: EmployeeService 등록 기능 구현
    - 사번 자동 생성 로직 구현 (EMP-연도-랜덤 5자리 형식, 중복 방지 포함)
    - 이메일 중복 검사 및 예외 처리
    - 부서 ID로 부서 엔티티 조회 및 예외 처리
    - Mapper 사용해 엔티티 생성 및 DTO 변환 처리

--- src/main/java/com/team6/hrbank/repository/EmployeeRepository.java
+++ src/main/java/com/team6/hrbank/repository/EmployeeRepository.java
@@ -6,10 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee save(Employee employee);
+
     Optional<Employee> findById(Long id);
+
     List<Employee> findAll();
+
     void deleteById(Long id);
+
     boolean existsByEmail(String email);
+
+    boolean existsByEmployeeNumber(String employeeNumber);
 }
--- src/main/java/com/team6/hrbank/service/FileMetadataService.java
+++ src/main/java/com/team6/hrbank/service/FileMetadataService.java
@@ -1,13 +1,16 @@
 package com.team6.hrbank.service;
 
 import com.team6.hrbank.entity.FileMetadata;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface FileMetadataService {
 
-  Optional<FileMetadata> findById(Long id);
+    Optional<FileMetadata> findById(Long id);
 
+    FileMetadata create(MultipartFile file);
 
 }
