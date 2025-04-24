407fe046837f64300ab530e0ab69dbd688305790
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-24 14:48:59 +0900

    feat: EmployeeStatsScheduler 클래스 구현 #95

---
+++ src/main/java/com/team6/hrbank/scheduler/EmployeeStatsScheduler.java
@@ -0,0 +1,21 @@
+package com.team6.hrbank.scheduler;
+
+
+import com.team6.hrbank.service.EmployeeStatsService;
+import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmployeeStatsScheduler {
+  private final EmployeeStatsService employeeStatsService;
+
+  // 오전 12시 5분에 배치를 날려서, 바로 직전 날의 데이터 반영
+  @Scheduled(cron = "0 05 00 * * *")
+  @SchedulerLock(name = "batchCreateEmployeeStats", lockAtMostFor = "10m")
+  public void batchCreateEmployeeStats() {
+    employeeStatsService.createTodayStats();
+  }
+}
