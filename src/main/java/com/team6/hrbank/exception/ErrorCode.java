93b0fa676f3f133df4833c44f1e9cb84161bd84b
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-24 13:37:05 +0900

    feat: EmployeeStats 배치 작업을 위한 ErrorCode 추가 #95

--- src/main/java/com/team6/hrbank/exception/ErrorCode.java
+++ src/main/java/com/team6/hrbank/exception/ErrorCode.java
@@ -21,7 +21,8 @@ public enum ErrorCode implements Code {
 
   //EMPLOYEESTATS
   UNSUPPORTED_UNIT(HttpStatus.BAD_REQUEST, "지원하지 않는 Unit 입니다."),
-  EMPLOYEE_STATS_NOT_FOUND(HttpStatus.NOT_FOUND, "직원 통계를 찾을 수 없습니다.");
+  EMPLOYEE_STATS_NOT_FOUND(HttpStatus.NOT_FOUND, "직원 통계를 찾을 수 없습니다."),
+  DUPLICATE_EMPLOYEESTATS(HttpStatus.CONFLICT, "해당 날짜의 직원 통계가 이미 존재합니다.");
 
   private final HttpStatus status;
   private final String message;
