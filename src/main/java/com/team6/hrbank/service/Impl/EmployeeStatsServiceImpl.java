459418608b6ab162e100fb2809b11b48374c81be
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-24 13:38:47 +0900

    feat: EmployeeStats 배치 작업을 위한 Service 구현체 작성 #95

--- src/main/java/com/team6/hrbank/service/Impl/EmployeeStatsServiceImpl.java
+++ src/main/java/com/team6/hrbank/service/Impl/EmployeeStatsServiceImpl.java
@@ -5,40 +5,74 @@ import com.team6.hrbank.entity.EmployeeStats;
 import com.team6.hrbank.exception.ErrorCode;
 import com.team6.hrbank.exception.RestException;
 import com.team6.hrbank.mapper.EmployeeStatsMapper;
+import com.team6.hrbank.repository.EmployeeQueryRepository;
 import com.team6.hrbank.repository.EmployeeStatsRepository;
 import com.team6.hrbank.service.EmployeeStatsService;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
 public class EmployeeStatsServiceImpl implements EmployeeStatsService {
 
   private final EmployeeStatsRepository employeeStatsRepository;
+  private final EmployeeQueryRepository employeeQueryRepository;
   private final EmployeeStatsMapper employeeStatsMapper;
 
 
   @Override
+  @Transactional
   public void createTodayStats() {
+    LocalDate currentDate = LocalDate.now(ZoneId.of("Asia/Seoul"));
+    LocalDate prevDate = currentDate.minusDays(1);
+    long currentCount = employeeQueryRepository.count();
+    if (employeeStatsRepository.existsByStatDate(currentDate)) {
+      log.error("중복 직원 통계 데이터 생성 시도: {}", currentDate);
+      throw new RestException(ErrorCode.DUPLICATE_EMPLOYEESTATS);
+    }
 
+    if (employeeStatsRepository.findByStatDate(prevDate).isEmpty()) {
+      long joinedCount = currentCount;
+      long leftCount = 0;
+      EmployeeStats currentEmployeeStats = createEmployeeStats(currentDate, currentCount,
+          joinedCount, leftCount);
+
+      saveEmployeeStats(currentEmployeeStats);
+      return;
+    }
+
+    long prevCount = employeeStatsRepository.findByStatDate(prevDate).get().getEmployeeCount();
+    long change = currentCount - prevCount;
+    long joinedCount = change > 0 ? change : 0;
+    long leftCount = change < 0 ? -change : 0;
+
+    EmployeeStats currentEmployeeStats = createEmployeeStats(currentDate, currentCount, joinedCount,
+        leftCount);
+
+    saveEmployeeStats(currentEmployeeStats);
   }
 
   @Override
   @Transactional(readOnly = true)
   public List<EmployeeTrendDto> getEmployeeTrend(LocalDate from, LocalDate to, String unit) {
     // null일 경우 기본값 지정
-    if(to == null) {
+    if (to == null) {
       to = LocalDate.now();
     }
 
     String unitUpper = unit.toUpperCase();
 
-    if(from == null) {
-      from = switch(unitUpper) {
+    if (from == null) {
+      from = switch (unitUpper) {
         case "DAY" -> to.minusDays(12);
         case "WEEK" -> to.minusWeeks(12);
         case "MONTH" -> to.minusMonths(12);
@@ -148,8 +182,30 @@ public class EmployeeStatsServiceImpl implements EmployeeStatsService {
   }
 
   private EmployeeStats findEmployeeStatsByStatDate(LocalDate statDate) {
+    log.error("직원 통계 조회 실패: {}", statDate);
     return employeeStatsRepository.findByStatDate(statDate)
         .orElseThrow(() -> new RestException(ErrorCode.EMPLOYEE_STATS_NOT_FOUND));
+  }
+
+  private void saveEmployeeStats(EmployeeStats stats) {
+    try {
+      // Unique 제약조건으로 인한 예외를 잡기 위해 바로 Flush
+      employeeStatsRepository.saveAndFlush(stats);
+    } catch (DataIntegrityViolationException e) {
+      log.error("중복 직원 통계 데이터 생성 시도: {}", stats.getStatDate());
+      throw new RestException(ErrorCode.DUPLICATE_EMPLOYEESTATS);
+    }
+  }
+
+  private EmployeeStats createEmployeeStats(LocalDate currentDate, long currentCount,
+      long joinedCount, long leftCount) {
+    return EmployeeStats.builder()
+        .statDate(currentDate)
+        .createdAt(Instant.now())
+        .employeeCount(currentCount)
+        .joinedCount(joinedCount)
+        .leftCount(leftCount)
+        .build();
   }
 
 }
