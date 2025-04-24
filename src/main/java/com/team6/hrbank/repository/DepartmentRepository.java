84049e4e51b5166de5662e4e3b1a1325af5f786a
최규원 <rbdnjs1011@naver.com>
2025-04-24 18:10:20 +0900

    feat: specification으로 인해Department repository 수정

--- src/main/java/com/team6/hrbank/repository/DepartmentRepository.java
+++ src/main/java/com/team6/hrbank/repository/DepartmentRepository.java
@@ -1,17 +1,17 @@
 package com.team6.hrbank.repository;
 
 import com.team6.hrbank.entity.Department;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long>,
+    JpaSpecificationExecutor<Department>{
   boolean existsByDepartmentName(String departmentName);
 
-  @Query("SELECT COUNT(e.id) FROM Employee e WHERE e.department.id = :departmentId")
-  Long countEmployees(@Param("departmentId") Long departmentId);
+  @Query("SELECT COUNT(e) FROM Employee e WHERE e.department.id = :departmentId")
+  Long countEmployeesByDepartmentId(@Param("departmentId") Long departmentId);
 }
