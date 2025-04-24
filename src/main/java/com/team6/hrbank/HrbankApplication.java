90f6b61c8ebb25a7dbacca732870f0450eb034a9
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-24 13:40:57 +0900

    feat: 스케쥴 작업 시 동시성 제어를 위한 shedlock 의존성 주입 #95

--- src/main/java/com/team6/hrbank/HrbankApplication.java
+++ src/main/java/com/team6/hrbank/HrbankApplication.java
@@ -1,10 +1,14 @@
 package com.team6.hrbank;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 @SpringBootApplication
 public class HrbankApplication {
 
