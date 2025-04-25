56288d478535e941673f6e19f866a63ab25ed0ae
hanna kong <khn1346@gmail.com>
2025-04-26 02:54:19 +0900

    feat: 직원 등록, 수정, 삭제 시 수정 이력 생성 로직 추가

--- src/main/java/com/team6/hrbank/service/EmployeeService.java
+++ src/main/java/com/team6/hrbank/service/EmployeeService.java
@@ -8,15 +8,15 @@ import java.time.LocalDate;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface EmployeeService {
-    EmployeeDto create(EmployeeCreateRequest request, MultipartFile profileImage);
+    EmployeeDto create(EmployeeCreateRequest request, MultipartFile profileImage, String ipAddress);
 
     EmployeeDto findById(Long id);
 
     CursorPageResponseEmployeeDto searchEmployees(EmployeeSearchCondition condition);
 
-    EmployeeDto update(Long id, EmployeeUpdateRequest request, MultipartFile profileImage);
+    EmployeeDto update(Long id, EmployeeUpdateRequest request, MultipartFile profileImage, String ipAddress);
 
-    void deleteById(Long id);
+    void deleteById(Long id, String ipAddress);
 
     long count(EmployeeState status, LocalDate fromDate, LocalDate toDate);
 }
