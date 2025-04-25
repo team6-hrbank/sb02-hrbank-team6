ac316387db200de96fd407733b081e3d3ecfa51a
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-25 10:33:42 +0900

    feat: EmployeeQueryRepository 직원 수 집계 쿼리메서드 추가 #167

--- src/main/java/com/team6/hrbank/repository/EmployeeQueryRepository.java
+++ src/main/java/com/team6/hrbank/repository/EmployeeQueryRepository.java
@@ -3,6 +3,7 @@ package com.team6.hrbank.repository;
 import com.team6.hrbank.entity.Employee;
 import com.team6.hrbank.entity.EmployeePosition;
 import com.team6.hrbank.entity.EmployeeState;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeQueryRepository extends JpaRepository<Employee, Long> {
@@ -10,9 +11,9 @@ public interface EmployeeQueryRepository extends JpaRepository<Employee, Long> {
 
   long countByEmployeePositionAndEmployeeState(EmployeePosition position, EmployeeState employeeState);
 
-  // 직원 수 집계 시 퇴직한 직원 제외
   long countByEmployeeStateNot(EmployeeState employeeState);
 
-  // 추후 총 직원 수 조회 API에 사용할 예정 (대시보드 위쪽)
-  // long countByEmployeeStateAndHireDateBetween(EmployeeState employeeState, LocalDate from, LocalDate to);
+  long countByEmployeeState(EmployeeState employeeState);
+
+  long countByEmployeeStateAndHireDateBetween(EmployeeState employeeState, LocalDate fromDate, LocalDate toDate);
 }
