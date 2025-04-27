5533693dde94e782bfa638aca4cfe306d45704f0
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-28 01:00:53 +0900

    refactor: dto record -> 일반 클래스 변경 #189

--- src/main/java/com/team6/hrbank/dto/employeestats/EmployeeTrendDto.java
+++ src/main/java/com/team6/hrbank/dto/employeestats/EmployeeTrendDto.java
@@ -1,12 +1,18 @@
 package com.team6.hrbank.dto.employeestats;
 
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record EmployeeTrendDto(
-    LocalDate date,
-    long count,
-    long change,
-    double changeRate
-) {
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeTrendDto {
+  private LocalDate date;
+  private long count;
+  private long change;
+  private double changeRate;
 }
