6c4910a997e65dbb8b4d412c292a98ddae6d221c
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-29 01:17:03 +0900

    refactor: Scheduler 시간 재설정 (매시간 22분 마다)

--- src/main/java/com/team6/hrbank/scheduler/DepartmentStatsScheduler.java
+++ src/main/java/com/team6/hrbank/scheduler/DepartmentStatsScheduler.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class DepartmentStatsScheduler {
   private final DepartmentStatsService departmentStatsService;
 
-  @Scheduled(cron = "0 15 15 * * *")
+  @Scheduled(cron = "0 22 * * * *")
   @SchedulerLock(name = "batchCreateDepartmentStats", lockAtMostFor = "10m")
   public void batchCreateDepartmentStats() {
     departmentStatsService.createTodayStats();
--- src/main/java/com/team6/hrbank/scheduler/EmployeeStatsScheduler.java
+++ src/main/java/com/team6/hrbank/scheduler/EmployeeStatsScheduler.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class EmployeeStatsScheduler {
   private final EmployeeStatsService employeeStatsService;
 
-  @Scheduled(cron = "0 15 15 * * *")
+  @Scheduled(cron = "0 22 * * * *")
   @SchedulerLock(name = "batchCreateEmployeeStats", lockAtMostFor = "10m")
   public void batchCreateEmployeeStats() {
     employeeStatsService.createTodayStats();
